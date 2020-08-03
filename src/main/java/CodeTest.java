98ccd39fa35de6461cd525419773936a80e0eab1
apadigal <anandkumar.padigala@gmail.com>
2020-08-03 19:20:45 +0100

    feat: this change includes the implementation of the methods and it's test cases chore: update JUnit version to 5.2

--- src/main/java/CodeTest.java
+++ src/main/java/CodeTest.java
@@ -1,39 +1,113 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingInt;
 
 public class CodeTest {
+    private static String[] ARGUMENTS;
     public static void main(String[] args) {
-        System.out.println("Please replace this with calls to all completed tests.");
+        ARGUMENTS = args;
+        System.out.println(String.format("reverseArray : %s", objectToString(reverseArray(new String[]{}))));
+        System.out.println(String.format("reverseArray : %s", objectToString(reverseArray(new String[]{"a", "b", "c", "d"}))));
+        System.out.println(String.format("uppercaseArray : %s", objectToString(uppercaseArray(new String[]{"a", "b", "c", "d"}))));
+        System.out.println(String.format("findWordCount : %s", findWordCount("Anand test this test with this test", "test")));
+        System.out.println(String.format("getCountOfWords : %s", getCountOfWords("Anand t est this test with this test")));
+
+        Function<Integer, Integer> composedFunc = composeU(x -> x /2, y -> y * y * y);
+        System.out.println(String.format("Compose of f1(f2(10)) = (10 * 10 * 10)/2 = %d", composedFunc.apply(10)));
+
+        System.out.println(composedFunc.apply(10));
+        writeContentsToConsole();
+        handleInvalidArgument();
+        puzzle(new String[]{"1", "2", "23", "7", "7", "8"});
     }
 
     public static String[] reverseArray(String[] input) {
-        // add code here
-        return input;
+        validateInput(input);
+        List<String> list = asList(input);
+        Collections.reverse(list);
+
+        return list.toArray(new String[0]);
     }
 
     public static String[] uppercaseArray(String[] input) {
-        // add code here
-        return input;
+        validateInput(input);
+        List<String> list = asList(input);
+        list = list.parallelStream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+        return list.toArray(new String[0]);
+    }
+
+    public static Map<String, Integer> getCountOfWords(String text) {
+        validateInput(text);
+        List<String> words =  Arrays.asList(text.split(" "));
+        return words.stream()
+                .collect(groupingBy(Function.identity(), summingInt(e -> 1)));
     }
 
     public static int findWordCount(String text, String wordToFind) {
+        List<String> words =  Arrays.asList(text.split(" "));
+        return Math.toIntExact(words.parallelStream()
+                .filter( s -> s.equals(wordToFind))
+                .count());
         // add code here
-        return 0;
     }
 
     public static Function<Integer,Integer> composeU(Function<Integer,Integer> f1, Function<Integer,Integer> f2){
-        // add code here
-        return null;
+        return f1.compose(f2);
     }
 
     public static void writeContentsToConsole() {
-        // add code here
+        String fileName = "test.properties";
+        Properties properties = new Properties();
+        try(InputStream inputStream = CodeTest.class.getResourceAsStream(fileName)){
+            properties.load(inputStream);
+            properties.entrySet().forEach(System.out::println);
+        }catch (IOException ex){
+            ex.printStackTrace();
+        };
     }
 
     public static void handleInvalidArgument() {
         // add code here
+        List<String> validArguments=Arrays.asList("check", "value", "class", "test", "now", "pension");
+        Arrays.stream(ARGUMENTS)
+                .filter(arg -> !validArguments.contains(arg))
+                .findAny()
+                .ifPresent(ch -> {
+                    throw new IllegalArgumentException("Only following command line arguments allowed : "+ objectToString(validArguments));
+                });
     }
 
-    public static void puzzle() {
+    public static void puzzle(String[] puzzleToTest) {
         // add code here
+        validateInput(puzzleToTest);
+        String currentValue = "";
+        final String SNAP = ",'Snap'";
+        for (String s : puzzleToTest) {
+            if (currentValue.equals(s)) {
+                System.out.print(s + SNAP);
+                break;
+            }
+            System.out.print(s + ",");
+            currentValue = s;
+        }
     }
-}
+
+    private static void validateInput(Object input){
+        requireNonNull(input, "Input value must be non null.");
+    }
+    private static String objectToString(Object obj) {
+        if (Objects.nonNull(obj) && obj.getClass().isArray()) {
+            return asList((Object[]) obj).toString();
+        } else {
+            return String.valueOf(obj);
+        }
+    }}
--- src/test/java/CodeTestSpec.java
+++ src/test/java/CodeTestSpec.java
@@ -1,5 +1,19 @@
-import org.junit.Test;
-import static org.junit.Assert.assertArrayEquals;
+//import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 
 /*
  *   Please code the tests in the format of reverseArray_returnsExpectedResult. This is an example of how we write our tests at Cardano.
@@ -17,7 +31,7 @@ public class CodeTestSpec {
         final String[] EXPECTED = {"x", "y", "z"};
 
         // act
-        final String[] actual = CodeTest.reverseArray(new String[] {"z", "y", "x"});
+        final String[] actual = CodeTest.reverseArray(new String[]{"z", "y", "x"});
 
         // assert
         assertArrayEquals(EXPECTED, actual);
@@ -25,31 +39,105 @@ public class CodeTestSpec {
 
     @Test
     public void uppercaseArray_returnsExpectedResult() {
+        // arrange
+        final String[] EXPECTED = {"X", "Y", "Z"};
+
+        // act
+        final String[] actual = CodeTest.uppercaseArray(new String[]{"x", "y", "z"});
+
+        // assert
+        assertArrayEquals(EXPECTED, actual);
 
     }
 
     @Test
     public void findWordCount_returnsExpectedResult() {
+        // arrange
+        final int EXPECTED = 3;
+
+        // act
+        final int actual = CodeTest.findWordCount("test test a test", "test");
+
+        // assert
+        assertEquals(EXPECTED, actual);
+
+    }
+
+    @Test
+    public void getCountOfWords_returnsExpectedResult() {
+        // arrange
+        final Map<String, Integer> EXPECTED = new HashMap<>();
+        EXPECTED.put("test", 3);
+        EXPECTED.put("a", 1);
+
+        // act
+        final Map<String, Integer>
+                actual = CodeTest.getCountOfWords("test test a test");
+
+        // assert
+        assertThat(EXPECTED, is(actual));
 
     }
 
     @Test
     public void composeU_returnsExpectedResult() {
+        // arrange
+        final int EXPECTED = 42;
 
+        //act
+        Function<Integer, Integer> halfNumber = x -> x / 2;
+        Function<Integer, Integer> customMath = y -> (y + 8) * y;
+        Function<Integer, Integer> composedFunctionTest = CodeTest.composeU(halfNumber, customMath);
+
+        // assert
+        assertThat(composedFunctionTest.apply(6), equalTo(EXPECTED));
     }
 
     @Test
     public void writeContentsToConsole_returnsExpectedResult() {
+        // arrange
+        final String EXPECTED = "prop2=value2\n" +
+                "prop1=value1\n" +
+                "prop4=value4\n" +
+                "prop3=value3\n";
 
+        //act
+        PrintStream oldStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        CodeTest.writeContentsToConsole();
+        String value = outputStream.toString();
+
+        //assert
+        assertEquals(EXPECTED, value);
+        System.setOut(oldStream);
     }
 
     @Test
     public void handleInvalidArgument_returnsExpectedResult() {
 
+        //assert?? just call the main method and check it's not throwing exception.
+        CodeTest.main(new String[]{"value", "test"});
+
+        // validate is IllegalArgumentException throws with expected message.
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> CodeTest.main(new String[]{"value1", "test"}));
+        assertEquals("Only following command line arguments allowed : [check, value, class, test, now, pension]", illegalArgumentException.getMessage());
+
     }
 
     @Test
     public void puzzle_returnsExpectedResult() {
+        // arrange
+        final String EXPECTED = "1,2,4,4,'Snap'";
 
+        //act
+        PrintStream oldStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        CodeTest.puzzle(new String[]{"1", "2", "4", "4", "8", "9"});
+        String value = outputStream.toString();
+
+        //assert
+        assertEquals(EXPECTED, value);
     }
 }
