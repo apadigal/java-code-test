f90fe1a9e51b48d4942702bac1a6b13a10ae62cd
apadigal <anandkumar.padigala@gmail.com>
2020-08-03 22:48:02 +0100

    fix: fixed select query to show only what requested for

--- queries.sql
+++ queries.sql
@@ -16,8 +16,8 @@ select col1, col2, col3, col4, sysdate
 
 -- 3.Create a query that joins two tables together. Note, all rows from the first table must be in the result-set
 -- (e.g. given customer and order tables, return all customers and any orders for each customer
-select c.*, o.*,
-       count(*) OVER (PARTITION BY o.customer_id) AS total_orders
+select c.*, o.*
+--       ,count(o.customer_id) OVER (PARTITION BY c.customer_id) AS total_orders
   from customer c
   left outer join order o
     on c.customer_id = o.customer_id;
